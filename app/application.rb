4111f4ef989e12606abf6138c45a854205c579b0
Rosie Choi <rosiechoimanso@gmail.com>
2021-04-03 21:12:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
 
     elsif req.path.match(/add/)
     search_term = req.params["item"]
-    resp.write handle_add(search_term
+    resp.write handle_add(search_term)
 
     else
       resp.write "Path Not Found"
